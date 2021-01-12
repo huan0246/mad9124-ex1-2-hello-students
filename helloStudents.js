773ccb78d5a0ddb9de15b3c4d685f527e040c7a2
Huang Chunyen <huan0246@algonquinlive.com>
2021-01-11 22:36:39 -0500

    Update js file

--- helloStudents.js
+++ helloStudents.js
@@ -1,9 +1,14 @@
 'use strict'
 
 // 1. Read the JSON file into a variable called students
-
+const students = require('./students.json');
+//console.log(students);
 // 2. Iterate over the students array and print Hello with their full names to the console
 // e.g. Hello Walter Baker
-
+students.forEach(({firstName, lastName}) => {
+  console.log(`Hello ${firstName} ${lastName}`);
+})
 // 3. Print out the number of last names starting with the letter D
 // e.g. Count of last names starting with D is 1
+const numberOfD = students.filter(({lastName}) => lastName.charAt(0) === 'D');
+console.log('Count of last names starting with D is ' + numberOfD.length);
